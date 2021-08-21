ee4996b693727f5f4f949516c84d2a39d73cf23e
Vedanta Somnathe <vedusom142005@gmail.com>
2021-08-21 05:40:05 +0530

    video calling bangaya bhai sahab

--- public/js/video-chat.js
+++ public/js/video-chat.js
@@ -9,6 +9,10 @@ newPeer.on("open", (id) => {
   socket.emit("join", roomID, id);
 });
 
+if (teech === "false") {
+  document.querySelector(".share-btn").remove();
+}
+
 // video
 const grid = document.querySelector(".video-grid");
 const userVideo = document.createElement("video");
@@ -49,17 +53,48 @@ async function videoSetup() {
   });
 
   //  ---------------------------------------------------------------- (stop audio and video)
-  for (let track of stream.getTracks()) {
-    if (track.kind === "audio") {
-      track.stop();
+  document.querySelector("#mic").addEventListener("click", () => {
+    if (document.querySelector("#mic").classList.contains("active")) {
+      for (let track of stream.getTracks()) {
+        if (track.kind === "audio") {
+          track.enabled = false;
+        }
+      }
+    } else {
+      for (let track of stream.getTracks()) {
+        if (track.kind === "audio") {
+          track.enabled = true;
+        }
+      }
     }
-  }
+
+    document.querySelector("#mic").classList.toggle("active");
+  });
+
+  document.querySelector("#vid").addEventListener("click", () => {
+    if (document.querySelector("#vid").classList.contains("active")) {
+      for (let track of stream.getTracks()) {
+        if (track.kind === "video") {
+          track.enabled = false;
+        }
+      }
+    } else {
+      for (let track of stream.getTracks()) {
+        if (track.kind === "video") {
+          track.enabled = true;
+        }
+      }
+    }
+
+    document.querySelector("#vid").classList.toggle("active");
+  });
 }
 videoSetup();
 
 function connectToNewUser(userID, stream) {
   const call = newPeer.call(userID, stream);
   const video = document.createElement("video");
+  console.log("called");
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
@@ -77,7 +112,10 @@ socket.on("disconnected", (userID) => {
 });
 
 // ---------------------------------------------------------------- Screen share click
-document.querySelector(".share-btn").addEventListener("click", async () => {
-  const screen = await navigator.mediaDevices.getDisplayMedia();
-  addVideoStream(shareVideo, screen);
-});
+if (document.querySelector(".share-btn")) {
+  document.querySelector(".share-btn").addEventListener("click", async () => {
+    document.querySelector(".teech").classList.remove("teech");
+    const screen = await navigator.mediaDevices.getDisplayMedia();
+    addVideoStream(shareVideo, screen);
+  });
+}
